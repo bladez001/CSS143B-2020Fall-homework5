7b75d737438865b95e1aead5e777908a68a6e365
Timothy Muresan <muresan_timothy@yahoo.com>
2020-11-15 01:09:18 -0800

    Pr1 & 2 done

--- src/main/java/Problem1/InOrderTraverse.java
+++ src/main/java/Problem1/InOrderTraverse.java
@@ -2,11 +2,34 @@ package Problem1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InOrderTraverse {
     public static List<Integer> inorderTraversalIterative(TreeNode<Integer> root) {
         // homework
         List<Integer> result = new ArrayList<>();
-        return result;  // place holder
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode temp = root;
+
+        while (temp != null) {
+            stack.push(temp);
+            temp = temp.left;
+        }
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add((Integer) node.val);
+            node = node.right;
+
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+        return result;
     }
 }
--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -2,11 +2,46 @@ package Problem1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+
 
 public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
         // homework
         List<List<Integer>> result = new ArrayList<>();
-        return result;  // place holder
+        Queue<TreeNode> queue = new LinkedList<>();
+        /*I chose to use Linked List for this problem. I chose
+          linked list because it works well with queue. The reason
+          it works well is because queue needs a structure that has
+          quick insertion and removal. Another reason is that it
+          does not have a fixed size.
+        */
+
+        if (root == null) {
+            return result;
+        }
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+                list.add((Integer) temp.val);
+
+                if (temp.left != null) {
+                    queue.add(temp.left);
+                }
+
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
     }
 }
